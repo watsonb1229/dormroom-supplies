297565e0d2c5ccc83e64f0a27e7ab4e161835b9a
nputnam-hu <noahputnam@gmail.com>
2018-01-09 23:21:38 -0500

    initial working code

---
+++ app/models/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    port: 3001,
+
+    // location of backend, do not include trailing /
+    apiUrl: 'http://localhost:3000',
+};
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const config = require('../app/models/config');
+
+router.get('/', (req, res, next) => {
+    return res.render('index');
+});
+
+module.exports = router;
